d6a133b2d7fc9dd611b5e882ce02851bf70f4271
i-media-dev <mrk_dev@i-media.ru>
2025-10-29 12:31:06 +0300

    PRO-105: fix main finally

--- timer/main.py
+++ timer/main.py
@@ -6,24 +6,21 @@ from timer.page_load import measure_main_page_load_time
 
 
 @time_of_script
-def main():
-    async def run_all_measurements():
-        tasks = []
+async def main():
+    tasks = []
 
-        for pharmacy in PHARMACIES:
-            output_file_main = f'{pharmacy}_main'
-            output_file_cart = f'{pharmacy}_cart'
-            url_main, url_cart = PHARMACIES[pharmacy]
+    for pharmacy in PHARMACIES:
+        output_file_main = f'{pharmacy}_main'
+        output_file_cart = f'{pharmacy}_cart'
+        url_main, url_cart = PHARMACIES[pharmacy]
 
-            tasks.append(measure_main_page_load_time(
-                url_main, output_file_main))
-            tasks.append(measure_main_page_load_time(
-                url_cart, output_file_cart))
+        tasks.append(measure_main_page_load_time(
+            url_main, output_file_main))
+        tasks.append(measure_main_page_load_time(
+            url_cart, output_file_cart))
 
-        await asyncio.gather(*tasks)
-
-    asyncio.run(run_all_measurements())
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
